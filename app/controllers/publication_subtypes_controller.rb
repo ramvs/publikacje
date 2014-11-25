5c23b52946b12d3f0432d251ffd303c84bc82d8c
grzk695 <grzk@outlook.com>
2014-11-26 00:33:06 +0100

    Rails sucks

--- app/controllers/publication_subtypes_controller.rb
+++ app/controllers/publication_subtypes_controller.rb
@@ -1,6 +1,5 @@
 class PublicationSubtypesController < ApplicationController
   before_action :set_publication_subtype, only: [:show, :edit, :update, :destroy]
-  before_action :setup_subtype , only: [:create , :update]
   # GET /publication_subtypes
   # GET /publication_subtypes.json
   def index
@@ -14,40 +13,42 @@ class PublicationSubtypesController < ApplicationController
 
   # GET /publication_subtypes/new
   def new
-    @publication_subtype = PublicationSubtype.new
-    @subtype_list = PublicationSubtype.pluck(:name)
-    @type_list = PublicationType.pluck(:name)
+    @subtype = PublicationSubtype.new
+    setup_type_options
   end
 
   # GET /publication_subtypes/1/edit
   def edit
-    @publication_subtype = PublicationSubtype.find(params[:id])
-    @subtype_list = PublicationSubtype.pluck(:name)
-    @type_list = PublicationType.pluck(:name)
+   @subtype = PublicationSubtype.find(params[:id])
+   setup_type_options
   end
 
   # POST /publication_subtypes
   # POST /publication_subtypes.json
   def create
    respond_to do |format|
+      @subtype = PublicationSubtype.new(name: params[:name], publication_type_id: PublicationType.getId(params[:kind]))
       if @subtype.save
         format.html { redirect_to publication_subtypes_path, notice: 'Publication subtype was successfully created.' }
         format.json { render :show, status: :created, location: @subtype }
       else
+        setup_type_options
+        flash[:danger] = @subtype.errors.full_messages
         format.html { render :new }
         format.json { render json: @subtype.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /publication_subtypes/1
-  # PATCH/PUT /publication_subtypes/1.json
-  def update
+  def update 
+    @subtype = PublicationSubtype.find(params[:id])
+    @subtype.publication_type_id = PublicationType.getId(params[:kind]) 
     respond_to do |format|
       if @subtype.save
         format.html { redirect_to @subtype, notice: 'Publication subtype was successfully updated.' }
         format.json { render :show, status: :ok, location: @subtype }
       else
+        setup_type_options
         format.html { render :edit }
         format.json { render json: @subtype.errors, status: :unprocessable_entity }
       end
@@ -57,7 +58,7 @@ class PublicationSubtypesController < ApplicationController
   # DELETE /publication_subtypes/1
   # DELETE /publication_subtypes/1.json
   def destroy
-    @publication_subtype.destroy
+    @subtype.destroy
     respond_to do |format|
       format.html { redirect_to publication_subtypes_url, notice: 'Publication subtype was successfully destroyed.' }
       format.json { head :no_content }
@@ -67,23 +68,16 @@ class PublicationSubtypesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_publication_subtype
-      @publication_subtype = PublicationSubtype.find(params[:id])
+      @subtype = PublicationSubtype.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def publication_subtype_params
       params.require(:publication_subtype).permit(:name,:publication_type_id)
     end
-
-    def setup_subtype
-      @subtype = PublicationSubtype.new
-      @subtype.name = params[:name]
-      type = PublicationType.where(name: params[:kind]).limit(1)
-      if type == nil
-        type = PublicationType.new(name: params[:kind])
-      else
-        type = type.first
-      end
-      @subtype.publication_type = type
+    
+    def setup_type_options
+      @subtype_list = PublicationSubtype.uniq.pluck(:name)
+      @type_list = PublicationType.pluck(:name)
     end
 end
--- app/models/publication_subtype.rb
+++ app/models/publication_subtype.rb
@@ -2,6 +2,9 @@ class PublicationSubtype < ActiveRecord::Base
 	has_many :publications
 	belongs_to :publication_type
 
+	validates_presence_of :name , :publication_type
+	validates_uniqueness_of :name , scope: :publication_type_id 
+
 	def publication_type_name
 		if publication_type==nil
 			""
