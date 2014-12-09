34b75d1cc6e0d33dc64fac93286dadcdf9d5df3a
grzk695 <grzk@outlook.com>
2014-12-09 18:16:15 +0100

    Cancan checks added to publications

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,14 +1,18 @@
 class AdminsController < ApplicationController
+	
 	def users
 		@users = User.where.not(id: current_user.id)
+		authorize! :read , User
 	end
 
 	def destroy_user
+		authorize! :destroy , User
 		User.destroy(params[:id])
 		redirect_to users_administrator_path
 	end
 
 	def edit_user
+		authorize! :edit , User
 		@user = User.find(params[:id])
 		if @user == nil
 			flash[:danger] = "User not found!"
@@ -17,6 +21,7 @@ class AdminsController < ApplicationController
 	end
 
 	def update_user
+		authorize! :edit , User
 		@user = User.find(params[:id])
 		if @user.update_attributes(user_params)
 			flash[:notice] = "Zapisano zmiany"
