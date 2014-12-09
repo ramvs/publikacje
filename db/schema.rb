45c8d1bf9fcf8e187a22d50535a446491501774a
Krystian <krystian.kawa1@gmail.com>
2014-12-09 14:18:40 +0100

    added mailer and cron job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125195718) do
+ActiveRecord::Schema.define(version: 20141209105513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20141125195718) do
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
     t.boolean  "approved",               default: false, null: false
+    t.boolean  "reminder"
   end
 
   add_index "users", ["approved"], name: "index_users_on_approved", using: :btree
